adc414760825a0056ba4f6350e7e8fa0197bc340
ipetrov <test@ya.ru>
2015-03-29 09:35:09 +0300

    Simple point collection drawing

--- Eigenvalues/DataSet.cs
+++ Eigenvalues/DataSet.cs
@@ -16,6 +16,8 @@ namespace Eigenvalues
                 double last_y = rand.Next(3, 7);
 
                 DataPoints[data_set] = new PointCollection();
+                GeometryGroup xaxis_geom1 = new GeometryGroup();
+
                 for (double x = 0; x <= 100; x += 10)
                 {
                     last_y += rand.Next(-10, 10) / 10.0;
@@ -23,15 +25,27 @@ namespace Eigenvalues
                     if (last_y > 10) last_y = 10;
                     Point p = new Point(x, last_y);
                     DataPoints[data_set].Add(WtoD(p));
-                }
+                    xaxis_geom1.Children.Add(
+                        new EllipseGeometry(WtoD(p), 3, 3)
+                        );
+                }                
 
+                /*
                 Polyline polyline = new Polyline();
                 polyline.StrokeThickness = 1;
                 polyline.Stroke = DataBrushes[data_set];
                 polyline.Points = DataPoints[data_set];
 
                 canGraph.Children.Add(polyline);
+                */
+                Path xaxis_path1 = new Path();
+                xaxis_path1.Fill = Brushes.Black;
+                xaxis_path1.Stroke = Brushes.Black;
+                xaxis_path1.Data = xaxis_geom1;
+
+                canGraph.Children.Add(xaxis_path1);                
             }
+            
         }
     }
 }
